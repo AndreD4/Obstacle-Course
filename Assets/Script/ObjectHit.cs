5cd278a3c2e04e1e08e4bb2de8c9ae028c64dda8
AndreD4 <andremai629@gmail.com>
2023-04-14 12:08:50 +0900

    consle log OnCollison to check if its responding

--- Assets/Script/ObjectHit.cs
+++ Assets/Script/ObjectHit.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  void OnCollisionEnter(Collision other) 
+  {
+    Debug.Log("Bumped into wall!");
+  }
 }
