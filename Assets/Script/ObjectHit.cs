85a9b832511354c45ac90d2ca519d865a6f0d090
AndreD4 <andremai629@gmail.com>
2023-04-14 13:51:56 +0900

    if statement only change color if player hits

--- Assets/Script/ObjectHit.cs
+++ Assets/Script/ObjectHit.cs
@@ -6,7 +6,10 @@ public class ObjectHit : MonoBehaviour
 {
   void OnCollisionEnter(Collision other) 
   {
-    Debug.Log("Bumped into wall!");
-    GetComponent<MeshRenderer>().material.color = Color.red;
+    if(other.gameObject.tag == "Player")
+    {
+      GetComponent<MeshRenderer>().material.color = Color.red;
+    }
   }
+
 }
