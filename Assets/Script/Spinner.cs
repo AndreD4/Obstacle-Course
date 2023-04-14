1466fb939ccdd9bb2395468bf56567530d668fd0
AndreD4 <andremai629@gmail.com>
2023-04-14 14:18:25 +0900

    script for an object to spin

---
+++ Assets/Script/Spinner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float yAngle = 1;
+    void Update()
+    {
+        transform.Rotate(0,yAngle,0);
+        {
+
+        }
+    }
+}
